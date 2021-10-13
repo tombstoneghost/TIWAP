b36ac9713eb0469556e654049407e3c7374b3495
Simardeep Singh <simardeepsngh1@gmail.com>
2021-10-13 18:59:41 +0530

    Dockerfile Created

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.8.2-slim
+
+WORKDIR /app
+COPY . /app
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5001
+
+ENTRYPOINT ["python"]
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -785,4 +785,5 @@ def after_request(response):
 
 # Execute Main
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context=context)
+    # app.run(debug=True, ssl_context=context)
+    app.run(host='127.0.0.1', port=5001, debug=True, ssl_context=context)

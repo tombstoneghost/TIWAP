e57cdc297e7de4f6d568af5003c78b40a3a824ed
Simardeep Singh <simardeepsngh1@gmail.com>
2021-10-13 19:17:37 +0530

    Host Updated

--- app.py
+++ app.py
@@ -786,4 +786,4 @@ def after_request(response):
 # Execute Main
 if __name__ == '__main__':
     # app.run(debug=True, ssl_context=context)
-    app.run(host='127.0.0.1', port=5001, debug=True, ssl_context=context)
+    app.run(host='0.0.0.0', port=5001, debug=True, ssl_context=context)
